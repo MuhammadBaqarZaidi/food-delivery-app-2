d969ba4f0fff8c1b9f52f00febf7ff11b0ab43db
MuhammadBaqarZaidi <786smbz@gmail.com>
2023-05-17 21:00:56 +0500

    ok

--- src/app/product/[slug]/page.js
+++ src/app/product/[slug]/page.js
@@ -4,74 +4,71 @@ import React, { Component } from 'react';
 import Image from 'next/image';
 import styles from './page.module.css';
 
-async function getData() {
-  const res = await fetch('https://api.example.com/...');
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
- 
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data');
-  }
- 
-  return res.json();
-}
- 
-export default async function Page() {
-  const data = await getData();
- 
-  return <main></main>;
-}
+// async function getData() {
+//   const res = await fetch('https://api.example.com/...');
+//   // The return value is *not* serialized
+//   // You can return Date, Map, Set, etc.
 
+//   // Recommendation: handle errors
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error('Failed to fetch data');
+//   }
 
-export async function getStaticProps({ params }) {
-  const searchResults = await getProducts(params.slug)
-  const coffeeProduct = searchResults[0]
-
-  return {
-    props: {
-      product: coffeeProduct,
-    },
-  }
-}
-
-export async function getStaticPaths() {
-  const coffeeProducts = await getProducts('coffee')
-  let fullPaths = []
-
-  for (let product of coffeeProducts) {
-    fullPaths.push({ params: { slug: product.id } })
-  }
-
-  return {
-    paths: fullPaths,
-    fallback: 'blocking',
-  }
-}
-
-
-// Return a list of `params` to populate the [slug] dynamic segment
-// export async function generateStaticParams() {
-//   const product = await fetch('../../utils/data').then((res) => res.json());
-
-//   return product.map((product) => ({
-//     slug: product.slug,
-//     name: product.name,
-//   }));
+//   return res.json();
 // }
 
+// export default async function Page() {
+//   const data = await getData();
 
+//   return <main></main>;
+// }
+
+// export async function getStaticProps({ params }) {
+//   const searchResults = await getProducts(params.slug)
+//   const coffeeProduct = searchResults[0]
+
+//   return {
+//     props: {
+//       product: coffeeProduct,
+//     },
+//   }
+// }
+
+// export async function getStaticPaths() {
+//   const coffeeProducts = await getProducts('coffee')
+//   let fullPaths = []
+
+//   for (let product of coffeeProducts) {
+//     fullPaths.push({ params: { slug: product.id } })
+//   }
+
+//   return {
+//     paths: fullPaths,
+//     fallback: 'blocking',
+//   }
+// }
+
+// Return a list of `params` to populate the [slug] dynamic segment
+export async function generateStaticParams() {
+  const product = await fetch('../../utils/data').then((res) => res.json());
+
+  return product.map((product) => ({
+    slug: product.slug,
+    name: product.name,
+  }));
+}
 
 // Multiple versions of this page will be statically generated
 // using the `params` returned by `generateStaticParams`
-export default function ProductPage({ product }) {
+export default async function ProductPage({ params }) {
   // , { params: { slug: string } }
-  // const { slug, name } = params;
+  const { slug, name } = params;
   // ...
   // if (!product) {
   //   return <div>Product Not Found</div>;
   // }
+  // const product = await generateStaticParams();
 
   return (
     <div className={styles.product}>
